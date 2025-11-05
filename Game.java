61952bb19721a0ce98513532d28e420244c6f3cd
david <dshlaifer@gmail.com>
2025-11-05 14:57:27 -0500

    made my game idea

--- Game.java
+++ Game.java
@@ -33,21 +33,21 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room outside, bombSite, baracades, hallway, defuserSite;
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        outside = new Room("outside the building");
+        bombSite = new Room("in bomb room");
+        baracades = new Room("a blocked of room");
+        hallway = new Room("A long hall to get to the defuser");
+        defuserSite = new Room("The room to defuse the bomb from exploding");
         
         // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        outside.setExits(null, bombSite, baracades, hallway);
+        bombSite.setExits(null, null, null, outside);
+        baracades.setExits(null, outside, null, null);
+        hallway.setExits(outside, defuserSite, null, null);
+        defuserSite.setExits(null, null, null, hallway);
 
         // start game outside
         currentRoom = outside;  
